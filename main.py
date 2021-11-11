f39ca8cb6a4f5e06713b7df609b2d8eb5050d5bf
11129 <gerald.bartoli@vtsd.com>
2021-11-11 11:45:52 -0500

    maze

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
 import turtle as trtl
 
-maze = trtl.Turtle()
+# Setup Variables
+numSides = 25
+length = 20
+doorSize = 40
+wallPiece = 5
+# Turtle Settings
+painter = trtl.Turtle()
+painter.speed("fastest")
+painter.pensize(5)
 
-maxloopside = 25
-a = 0
-length = 10
+currentSide = 0
+currentSize = length
+while (currentSide < numSides):
+    if (currentSide < 4):
+        painter.forward(currentSize)
+        painter.left(90)
 
-maze.left(90)
+    else:
+        painter.forward(wallPiece)
+        painter.penup()
+        painter.forward(doorSize)
+        painter.pendown()
+        painter.left(90)
+        painter.forward(length)
+        painter.right(180)
+        painter.forward(length)
+        painter.left(90)
+        painter.forward(currentSize - doorSize - wallPiece)
 
-while (a < maxloopside):
-    maze.forward(50)
-    maze.right(90)
+    painter.left(90)
+    currentSize += length
+    currentSide+=1
 
+
+painter.hideturtle()
 
 wn = trtl.Screen()
 wn.mainloop()
