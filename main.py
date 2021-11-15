242e7043c7677a4c9e6321baa51d0a615f7a4cd7
11129 <gerald.bartoli@vtsd.com>
2021-11-15 13:12:06 -0500

    maze

--- main.py
+++ main.py
@@ -1,38 +1,79 @@
+# Imports
 import turtle as trtl
+import random as rand
 
-# Setup Variables
+#Setup Variables
 numSides = 25
 length = 20
+color = "purple"
 doorSize = 40
-wallPiece = 5
-# Turtle Settings
+beforeDoor = 5
+
+#Turtle Settings
 painter = trtl.Turtle()
+painter.pencolor(color)
 painter.speed("fastest")
 painter.pensize(5)
 
+
 currentSide = 0
 currentSize = length
-while (currentSide < numSides):
-    if (currentSide < 4):
-        painter.forward(currentSize)
-        painter.left(90)
+preBarrier = 40
 
+while(currentSide < numSides):
+    if(currentSide < 4):
+        #painter.forward(currentSize)
+        print("")
     else:
-        painter.forward(wallPiece)
-        painter.penup()
-        painter.forward(doorSize)
-        painter.pendown()
-        painter.left(90)
-        painter.forward(length)
-        painter.right(180)
-        painter.forward(length)
-        painter.left(90)
-        painter.forward(currentSize - doorSize - wallPiece)
+        beforeDoor = rand.randint(length, currentSize - doorSize-length)
+        prebarrier = rand.randint(length, currentSize - length - beforeDoor)
+
+
+        if(beforeDoor < prebarrier):
+
+            # Door Handle(r)
+            painter.forward(beforeDoor)
+            #painter.penup()
+            painter.pencolor("red")
+            painter.forward(doorSize)
+            painter.pencolor(color)
+            #painter.pendown()
+
+            # CFM Code Here
+            painter.forward(preBarrier)
+            painter.left(90)
+            painter.forward(length*2)
+            painter.right(180)
+            painter.forward(length*2)
+            painter.left(90)
+            # Wall End
+        else:
+
+            prebarrier = 40
+            beforeDoor = length * 2
+            # Door Handle(r)
+            painter.forward(beforeDoor)
+            # painter.penup()
+            painter.pencolor("red")
+            painter.forward(doorSize)
+            painter.pencolor(color)
+            # painter.pendown()
+
+            # CFM Code Here
+            painter.forward(preBarrier)
+            painter.left(90)
+            painter.forward(length * 2)
+            painter.right(180)
+            painter.forward(length * 2)
+            painter.left(90)
+            # Wall End
+
+
+        painter.forward(currentSize - doorSize - beforeDoor - preBarrier)
 
     painter.left(90)
     currentSize += length
     currentSide+=1
-
 
 painter.hideturtle()
 
